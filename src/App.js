81aab9a6e2a921cfb3a2bf55586d118d2722106e
Elvis Omoro <iamomoroit@gmail.com>
2022-11-01 20:06:37 +0300

    set canvas with + complete

--- src/App.js
+++ src/App.js
@@ -7,41 +7,43 @@ function App() {
   const [hasPhoto, setHasPhoto] = useState(false);
 
   const getVideo = () => {
-    navigator.mediaDevices.getUserMedia({
-      video: { width: 1920, height: 1080 },
-    })
-    .then(stream => {
-      let video =videoRef.current;
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(err => {console.error(err);})
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1920, height: 1080 },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   const takePhoto = () => {
     const width = 414;
-    const height = width / (16/9);
+    const height = width / (16 / 9);
 
     let video = videoRef.current;
-    let photo =photoRef.current;
+    let photo = photoRef.current;
 
     photo.width = width;
-    photo.height = height
+    photo.height = height;
 
-    let ctx = photo.getContext("2d");
+    let ctx = photo.getContext('2d');
     ctx.drawImage(video, 0, 0, width, height);
-    setHasPhoto(true)
-  }
+    setHasPhoto(true);
+  };
 
   const closePhoto = () => {
     let photo = photoRef.current;
-    let ctx = photo.getContext("2d");
+    let ctx = photo.getContext('2d');
 
-    ctx.clearRect(0,0, photo.width, photo.height);
-    
+    ctx.clearRect(0, 0, photo.width, photo.height);
+
     setHasPhoto(false);
-  }
-
+  };
 
   useEffect(() => {
     getVideo();
@@ -53,7 +55,7 @@ function App() {
         <video ref={videoRef}></video>
         <button onClick={takePhoto}>SNAP!</button>
       </div>
-      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+      <div className={'result ' + (hasPhoto ? 'hasPhoto' : '')}>
         <canvas ref={photoRef}></canvas>
         <button onClick={closePhoto}>CLOSE</button>
       </div>
